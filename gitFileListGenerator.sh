61593041724ef72f12699975ee6442e470c928e9
toe <4287898+toe-codes@users.noreply.github.com>
2017-11-12 00:58:48 +0900

    Modify gitFileListGenerator.sh
    Delete some unnecessary lines to print.

--- gitFileListGenerator.sh
+++ gitFileListGenerator.sh
@@ -9,6 +9,4 @@ git diff -w --numstat $HASH_BEFORE $HASH_AFTER > $SCRIPT_DIR/tmp/numstat.txt
 git diff --name-status $HASH_BEFORE $HASH_AFTER > $SCRIPT_DIR/tmp/name-status.txt
 git ls-tree -r --name-only $HASH_AFTER > $SCRIPT_DIR/tmp/ls-files.txt
 popd
-echo $SCRIPT_DIR
-pwd
 rscript $SCRIPT_DIR/module/createCSV.r $SCRIPT_DIR $SCRIPT_DIR/tmp/numstat.txt $SCRIPT_DIR/tmp/name-status.txt $SCRIPT_DIR/tmp/ls-files.txt
